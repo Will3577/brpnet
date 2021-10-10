be79b0e58f37ccf1baa58a155d5448c7cd65d81f
Hengrui  Wang <u6202156@anu.edu.au>
2021-10-10 12:44:51 +1100

    test

--- tafe/dataset.py
+++ tafe/dataset.py
@@ -49,7 +49,7 @@ class KumarDataset(torch.utils.data.Dataset):
         # self.seg_labels[self.seg_labels>0] = 1
 
         self.bnd_labels = np.load(os.path.join(loc_head, 'bnd.npy')).item()['bnds']
-        print(self.bnd_labels)
+        # print(self.bnd_labels)
         # self.bnd_labels = self.bnd_labels[list]
         
         self.bnd_labels = [self.bnd_labels[i] for i in list]
