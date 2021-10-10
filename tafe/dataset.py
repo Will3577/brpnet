9cc5572543fc94de460c6a5235ed2f6c0e22c23a
Hengrui  Wang <u6202156@anu.edu.au>
2021-10-11 00:33:49 +1100

    test post_proc.py

--- tafe/dataset.py
+++ tafe/dataset.py
@@ -39,7 +39,7 @@ class KumarDataset(torch.utils.data.Dataset):
         self.imgs = np.load(os.path.join(loc_head, 'data_after_stain_norm_ref1.npy'),allow_pickle=True).item()['train_imgs']
         # print(list,type(list),self.imgs,self.imgs.shape)
         # self.imgs = self.imgs[list]
-        self.imgs = [self.imgs[i] for i in list]
+        self.imgs = np.array([self.imgs[i] for i in list])
 
         self.seg_labels = np.load(os.path.join(loc_head, 'gt.npy'),allow_pickle=True).item()['segs']
         # print(self.seg_labels)
@@ -48,11 +48,11 @@ class KumarDataset(torch.utils.data.Dataset):
         # Instance labels to segmentation labels
         self.seg_labels[self.seg_labels>0] = 1
 
-        self.bnd_labels = np.load(os.path.join(loc_head, 'bnd.npy'))#,allow_pickle=True).item()['bnds']
+        self.bnd_labels = np.load(os.path.join(loc_head, 'bnd.npy'),allow_pickle=True).item()['bnds']
         # print(self.bnd_labels)
-        self.bnd_labels = self.bnd_labels[list]
+        # self.bnd_labels = self.bnd_labels[list]
         
-        # self.bnd_labels = [self.bnd_labels[i] for i in list]
+        self.bnd_labels = np.array([self.bnd_labels[i] for i in list])
         
         self.naug = 6#ori*1 + rotate*3 + flip*2
         self.nimg = len(self.imgs)
